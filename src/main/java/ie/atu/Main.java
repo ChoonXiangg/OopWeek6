678d152f2ada9de5eb879359d1ccabb80a7c46c8
ChoonXiang <g00425056@atu.ie>
2023-11-06 17:42:43 +0000

    Added array

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,28 +1,28 @@
 package ie.atu;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         int[] anArray;
         int i;
-        anArray = new int[10];
-        for (i = 0; i <= 9; i++) {
-            anArray[i] = 100 * (i + 1);
+        anArray=new int[10];
+        for(i=0;i<=9;i++){
+            anArray[i]=100*(i+1);
         }
-        int value1 = 0;
-        method1(anArray, value1);
-        Scanner scanner1 = new Scanner(System.in);
-        try {
+        int value1=0;
+        method1(anArray,value1);
+        Scanner scanner1=new Scanner(System.in);
+        try{
             System.out.println("Enter a new value: ");
-            anArray[10] = scanner1.nextInt();
+            anArray[10]=scanner1.nextInt();
         }
-        catch (ArrayIndexOutOfBoundsException e) {
+        catch(ArrayIndexOutOfBoundsException e){
             System.out.println("Array index out of bounds");
         }
     }
-    static void method1(int[] anArray, int value1) {
-        for (int value : anArray) {
-            System.out.println("Element at index " + value1 + ": " + value);
+    static void method1(int[] anArray,int value1){
+        for(int value:anArray){
+            System.out.println("Element at index "+value1+": "+value);
             value1++;
         }
     }
